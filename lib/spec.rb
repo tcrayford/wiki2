0c3b33cedd833fea4924cffe5b2c2de29571c1cb
Tom Crayford <tcrayford@googlemail.com>
2010-07-17 12:40:00 +0100

    Made expect_equals error messages better using inspect

--- lib/spec.rb
+++ lib/spec.rb
@@ -2,7 +2,7 @@ class ExpectationError < StandardError
 end
 
 def expect_equal(expected,actual,message='')
-  expect expected==actual, "#{expected} not equal to #{actual} #{", #{message}"}"
+  expect expected==actual, "#{expected.inspect} not equal to #{actual.inspect} #{", #{message}"}"
 end
 
 def expect(bool,message='')
@@ -16,7 +16,7 @@ end
 def expect_raises(exception)
   begin
     yield
-    expect false, "expect_raises fails"
+    expect false, "expect_raises failed"
   rescue exception => e #expected
   end
 end
@@ -25,11 +25,10 @@ def describe(message='')
   begin
     yield
   rescue ExpectationError => e
-    puts "Failure: #{e.message} #{message}"
+    puts "Failure: #{e.message} #{message}\n#{e.backtrace.join("\n")}"
   end
 end
 
 alias :it :describe
 alias :context :describe
 alias :before :describe
-
